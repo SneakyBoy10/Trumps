eac32ba3a62b62f90ba728d7046b5f28f8f9fbc1
burtho14 <tomalfieb10@gmail.com>
2019-10-29 14:51:23 +0000

    Base

---
+++ Trumps/Trumps/MainForm.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace Trumps
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.CardBox = new System.Windows.Forms.GroupBox();
+            this.Card7 = new System.Windows.Forms.PictureBox();
+            this.Card6 = new System.Windows.Forms.PictureBox();
+            this.Card5 = new System.Windows.Forms.PictureBox();
+            this.Card4 = new System.Windows.Forms.PictureBox();
+            this.Card3 = new System.Windows.Forms.PictureBox();
+            this.Card2 = new System.Windows.Forms.PictureBox();
+            this.Card1 = new System.Windows.Forms.PictureBox();
+            this.CardBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Card7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CardBox
+            // 
+            this.CardBox.Controls.Add(this.Card7);
+            this.CardBox.Controls.Add(this.Card6);
+            this.CardBox.Controls.Add(this.Card5);
+            this.CardBox.Controls.Add(this.Card4);
+            this.CardBox.Controls.Add(this.Card3);
+            this.CardBox.Controls.Add(this.Card2);
+            this.CardBox.Controls.Add(this.Card1);
+            this.CardBox.Location = new System.Drawing.Point(0, 0);
+            this.CardBox.Name = "CardBox";
+            this.CardBox.Size = new System.Drawing.Size(760, 230);
+            this.CardBox.TabIndex = 0;
+            this.CardBox.TabStop = false;
+            this.CardBox.Text = "Card Box";
+            // 
+            // Card7
+            // 
+            this.Card7.Image = ((System.Drawing.Image)(resources.GetObject("Card7.Image")));
+            this.Card7.Location = new System.Drawing.Point(648, 19);
+            this.Card7.Name = "Card7";
+            this.Card7.Size = new System.Drawing.Size(100, 200);
+            this.Card7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Card7.TabIndex = 6;
+            this.Card7.TabStop = false;
+            // 
+            // Card6
+            // 
+            this.Card6.Image = ((System.Drawing.Image)(resources.GetObject("Card6.Image")));
+            this.Card6.Location = new System.Drawing.Point(542, 19);
+            this.Card6.Name = "Card6";
+            this.Card6.Size = new System.Drawing.Size(100, 200);
+            this.Card6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Card6.TabIndex = 5;
+            this.Card6.TabStop = false;
+            // 
+            // Card5
+            // 
+            this.Card5.Image = ((System.Drawing.Image)(resources.GetObject("Card5.Image")));
+            this.Card5.Location = new System.Drawing.Point(436, 19);
+            this.Card5.Name = "Card5";
+            this.Card5.Size = new System.Drawing.Size(100, 200);
+            this.Card5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Card5.TabIndex = 4;
+            this.Card5.TabStop = false;
+            // 
+            // Card4
+            // 
+            this.Card4.Image = ((System.Drawing.Image)(resources.GetObject("Card4.Image")));
+            this.Card4.Location = new System.Drawing.Point(330, 19);
+            this.Card4.Name = "Card4";
+            this.Card4.Size = new System.Drawing.Size(100, 200);
+            this.Card4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Card4.TabIndex = 3;
+            this.Card4.TabStop = false;
+            // 
+            // Card3
+            // 
+            this.Card3.Image = ((System.Drawing.Image)(resources.GetObject("Card3.Image")));
+            this.Card3.Location = new System.Drawing.Point(224, 19);
+            this.Card3.Name = "Card3";
+            this.Card3.Size = new System.Drawing.Size(100, 200);
+            this.Card3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Card3.TabIndex = 2;
+            this.Card3.TabStop = false;
+            // 
+            // Card2
+            // 
+            this.Card2.Image = ((System.Drawing.Image)(resources.GetObject("Card2.Image")));
+            this.Card2.Location = new System.Drawing.Point(118, 19);
+            this.Card2.Name = "Card2";
+            this.Card2.Size = new System.Drawing.Size(100, 200);
+            this.Card2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Card2.TabIndex = 1;
+            this.Card2.TabStop = false;
+            // 
+            // Card1
+            // 
+            this.Card1.Image = ((System.Drawing.Image)(resources.GetObject("Card1.Image")));
+            this.Card1.Location = new System.Drawing.Point(12, 19);
+            this.Card1.Name = "Card1";
+            this.Card1.Size = new System.Drawing.Size(100, 200);
+            this.Card1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Card1.TabIndex = 0;
+            this.Card1.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.CardBox);
+            this.Name = "MainForm";
+            this.Text = "Main Form";
+            this.CardBox.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Card7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox CardBox;
+        private System.Windows.Forms.PictureBox Card7;
+        private System.Windows.Forms.PictureBox Card6;
+        private System.Windows.Forms.PictureBox Card5;
+        private System.Windows.Forms.PictureBox Card4;
+        private System.Windows.Forms.PictureBox Card3;
+        private System.Windows.Forms.PictureBox Card2;
+        private System.Windows.Forms.PictureBox Card1;
+    }
+}
+
